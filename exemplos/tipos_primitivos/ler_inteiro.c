3c68cc3c50fd82409281a7a0ef27b1aa99c3969f
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2020-11-30 11:35:24 -0300

    Exemplo tipos primitivos e tipo int

---
+++ exemplos/tipos_primitivos/ler_inteiro.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+    int num;
+    printf("Introduza um número inteiro: ");
+    scanf("%d",&num);
+    printf("num=%d\n",num);
+
+}
---
+++ exemplos/tipos_primitivos/resto_divisao.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    
+    int num=21/4;
+    int rest=21%4;
+    
+    printf("21/4=%d 21%%4=%d\n",num,rest);
+}
