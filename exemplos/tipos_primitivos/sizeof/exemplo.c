d33b36ba6fd970a4f63fe96b500d5ec20ee5a2f4
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2020-12-20 20:17:06 -0300

    Operador sizeof exemplo

---
+++ exemplos/tipos_primitivos/sizeof/exemplo.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void){
+
+	int i;
+
+	printf("O tamanho em bytes de i é %ld\n",sizeof(int));
+}
---
+++ exemplos/tipos_primitivos/sizeof/exemplo2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+
+	int i;
+	float f;
+	double d;
+	char c;
+
+	printf("O tamanho em bytes de i é %ld\n",sizeof(int));
+	printf("O tamanho em bytes de f é %ld\n",sizeof(float));
+	printf("O tamanho em bytes de d é %ld\n",sizeof(double));
+	printf("O tamanho em bytes de c é %ld\n",sizeof(char));
+}
