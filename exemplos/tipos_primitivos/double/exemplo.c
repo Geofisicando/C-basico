9b53a586c6c168607418cff05f5a50cdb3f11a36
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2020-12-20 22:04:02 -0300

    exemplo float e double

---
+++ exemplos/tipos_primitivos/double/exemplo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+
+	double pi=3.1415927;
+	double raio;
+
+	printf("Introduza o raio da circunferência: "); scanf("%lf",&raio);
+	printf("área=%lf\n",pi*raio*raio);
+
+}
---
+++ exemplos/tipos_primitivos/float/exemplo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+
+	float pi=3.1415927;
+	float raio;
+
+	printf("Introduza o raio da circunferência: "); scanf("%f",&raio);
+	printf("área=%f\n",pi*raio*raio);
+
+}
