8b284590f13584716a5c98183822f1c67bf60681
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2021-01-06 22:59:19 -0300

    Resolução exercício 1 sobre tipos primitivos

---
+++ exemplos/tipos_primitivos/exercicio_1/exercicio_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+
+	int n1, n2;
+
+	printf("Introduza dois números inteiros: ");
+	scanf("%d %d",&n1,&n2);
+
+	printf("n1+n2=%d\n",n1+n2);
+	printf("n1-n2=%d\n",n1-n2);
+	printf("n1*n2=%d\n",n1*n2);
+	printf("n1/n2=%d\n",n1/n2);
+	printf("n1%%n2=%d\n",n1%n2);
+}
